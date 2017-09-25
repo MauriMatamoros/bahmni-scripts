61994bc45a20b7f4c0212b04fa4c9556e2253d5a
MauriMatamoros <mauricio.enrique.matamoros@gmail.com>
2017-09-25 16:41:18 -0600

    Adding promises

--- app.js
+++ app.js
@@ -1,71 +1,15 @@
 const request = require('request');
+const rpn = require('request-promise-native');
 
 const { openmrsApi } = require('./openmrsApiCalls');
 
 process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '0';
 
-request(openmrsApi.postLocalConcept, {form: {
-  "set": "",
-  "answers": "",
-  "lowNormal": "",
-  "lowCritical": "",
-  "units": "",
-  "allowDecimal": "",
-  "descriptions": "First Post Script",
-  "version": "",
-  "mappings": "",
-  "names": "PostTest",
-  "displayPrecision": "",
-  "datatype": "Text",
-  "lowAbsolute": "",
-  "conceptClass": "Misc",
-  "setMembers": "",
-  "hiAbsolute": "",
-  "hiNormal": "",
-  "hiCritical": ""
-}});
-
-// request(openmrsApi.getAllLocalConcepts, (error, response, body) => {
-//   if (error) {
-//     console.log(error);
-//     return;
-//   }
-//   let counter  = 0;
-//   let concepts = body.results;
-//   concepts.forEach((concept) => {
-//     console.log(concept.uuid);
-//     console.log(concept.display);
-//     counter++;
-//   });
-//   console.log(counter);
-// });
-
-// let uuid = 'c393fd1d-3f10-11e4-adec-0800271c1b75';
-//
-// request(openmrsApi.getLocalConcept(uuid), (error, response, body) => {
-//   if (error) {
-//     console.log(error);
-//     return;
-//   }
-//   console.log(body);
-// });
-
-// request(openmrsApi.getLocalSystemSettings, (error, response, body) => {
-//   if (error) {
-//     console.log(error);
-//     return;
-//   }
-//   let results = body.results;
-//   results.forEach((result) => {
-//     console.log(result);
-//   })
-// });
-
-// let uuid = '47ba7340-88b0-4784-a305-0a935f53d96d';
-// request(openmrsApi.getLocalSystemSetting(uuid), (error, response, body) => {
-//   if (error) {
-//     console.log(error);
-//     return;
-//   }
-//   console.log(body);
-// });
+let uuid = '34e7c04c-914c-445e-a077-47e9da53304b';
+rpn(openmrsApi.getLocalConcept(uuid)).then((response) => {
+  let concept = JSON.stringify(response);
+  openmrsApi.postLocalConcept.json = concept;
+  rpn(openmrsApi.getLocalConcept).then((response) => {
+    console.log(response);
+  });
+});
