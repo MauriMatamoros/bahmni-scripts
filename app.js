66fc62ae1f8944e1128c51a9bf9196aeb3227bb2
MauriMatamoros <mauricio.enrique.matamoros@gmail.com>
2017-09-24 21:43:54 -0600

    Initial Commit

---
+++ app.js
@@ -0,0 +1,50 @@
+const request = require('request');
+
+const { openmrsApi } = require('./openmrsApiCalls');
+
+process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '0';
+
+// request(openmrsApi.getAllLocalConcepts, (error, response, body) => {
+//   if (error) {
+//     console.log(error);
+//     return;
+//   }
+//   let counter  = 0;
+//   let concepts = body.results;
+//   concepts.forEach((concept) => {
+//     console.log(concept.uuid);
+//     console.log(concept.display);
+//     counter++;
+//   });
+//   console.log(counter);
+// });
+
+// let uuid = 'c393fd1d-3f10-11e4-adec-0800271c1b75';
+//
+// request(openmrsApi.getLocalConcept(uuid), (error, response, body) => {
+//   if (error) {
+//     console.log(error);
+//     return;
+//   }
+//   console.log(body);
+// });
+
+// request(openmrsApi.getLocalSystemSettings, (error, response, body) => {
+//   if (error) {
+//     console.log(error);
+//     return;
+//   }
+//   let results = body.results;
+//   results.forEach((result) => {
+//     console.log(result);
+//   })
+// });
+
+let uuid = '47ba7340-88b0-4784-a305-0a935f53d96d';
+request(openmrsApi.getLocalSystemSetting(uuid), (error, response, body) => {
+  if (error) {
+    console.log(error);
+    return;
+  }
+  console.log(body);
+});
---
+++ openmrsApiCalls.js
@@ -0,0 +1,38 @@
+const baseUrl = 'https://192.168.33.10/openmrs/ws/rest/v1';
+const headers = { Authorization: `Basic `+Buffer.from('Superman:Admin123').toString('base64') };
+
+let openmrsApi = {
+  baseUrl,
+  getAllLocalConcepts: {
+    method: 'GET',
+    headers,
+    url: `${baseUrl}/concept`,
+    json: true
+  },
+  getLocalConcept(uuid) {
+    return getLocalConcept = {
+      method: 'GET',
+      headers,
+      url: `${baseUrl}/concept/${uuid}`,
+      json: true
+    };
+  },
+  getLocalSystemSettings: {
+    method: 'GET',
+    headers,
+    url: `${baseUrl}/systemsetting`,
+    json: true
+  },
+  getLocalSystemSetting(uuid) {
+    return getLocalConcept = {
+      method: 'GET',
+      headers,
+      url: `${baseUrl}/systemsetting/${uuid}`,
+      json: true
+    };
+  }
+};
+
+module.exports = {
+  openmrsApi
+};
